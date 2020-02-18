54890a909d887ca4147217f0a5f82518bb8ff425
Aleix Algueró <59015816+stv-beep@users.noreply.github.com>
2020-02-18 11:45:10 +0100

    Actualització 1.0.3
    Localitzar, recuperar, primerDarrer i imprimir implementats

--- funcions.java
+++ funcions.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 public class funcions {
 public static Scanner sc = new Scanner(System.in);
 public static String opcio;
-public static final int mida = 10;
+public final int mida = 10;
 public static String[] cognoms = {"Algueró","Ferre","Bayo","Falco","Fernandez","Didouh","Garcia","El Khattabi","Aloy","Brull"};//Elements:10
 public static boolean running = true;
 public static String element, x; //element
 public static int pos, p; //posició
-public static int num_elements;
+public static int num_elements; //comptador d'elements
+public static String nomllista, llista; //nom de la llista
+public static String primerDarrer;
 
 
 /*****FUNCIONS****/
@@ -23,11 +25,13 @@ String opcions[] = new String [12];
 opcions[0] = "\"i\": Inserir (element,posició) --- NO FUNCIONA";
 opcions[1] = "\"l\": Localitzar (element)      --- NO FUNCIONA BÉ";
 opcions[2] = "\"r\": Recuperar (posició)       --- OPERATIU";
+opcions[6] = "\"p\": PrimerDarrer (llista)     --- NO FUNCIONA BÉ";
+opcions[7] = "\"m\": Imprimir                  --- NO OPERATIU";
 for (String z : opcions) {
   if (z!=null) {
   System.out.println(z);}
 }
-opcio = sc.next();
+
 return;}
 
 /*Demanar element*/
@@ -46,10 +50,62 @@ public static int entradaPos() { //funció que retornarà posició entrada pel u
 
 }
 
+/*Demanar llista*/
+public static void entradaLlista() {
+  System.out.println("Introdueix una llista: ");
+  while(true) {
+      nomllista = sc.nextLine();
+      if (nomllista.contains("cognoms")) {
+        break;
+      }else{
+          System.out.println("Aquesta llista no existeix, torna a introduir una altra."+  " (cognoms) " );
+      }
+  }
+  return;
+}
+
+
+
+/*Funció PrimerDarrer*/
+public static void funcioPD() {
+  do {
+      System.out.println( "Opcions: \"primer\" o \"darrer\"" );
+      primerDarrer = sc.next();
+  } while(!(primerDarrer.equals("primer")||primerDarrer.equals("darrer")));
+  if (primerDarrer.equals("primer")) {
+      System.out.println(cognoms[0]);
+  } else if (primerDarrer.equals("darrer")) {
+      System.out.println(cognoms[num_elements-1] );
+  }
+  return;
+}
+
+/*Funció imprimir llista*/
+public static void funcioImpr() {
+if (num_elements > 0) {
+  int num = 0;
+for (String j : cognoms) {
+
+  num++;
+  System.out.println("L'element "+num+" és "+j+".");
+}
+} else {
+  System.out.println("Llista buida!");
+}
+
+return;
+}
+
+
+
+
+
+/*MAIN*/
 public static void main(String[] args){
   while (running) {
 
 Menu();
+opcio = sc.next();
 
 /*COMPTADOR. S'ha de canviar de puesto i posar-lo a cada operació, o fer una funció en ell*/
 num_elements = 0;//per a que comenci a comptar des del principi i per a que no es sumin elements de més cada vegada que es completi el bucle for.
@@ -61,7 +117,7 @@ for (String e : cognoms) {
 
 switch(opcio) {
 case "i":
-System.out.println("cas 1");
+System.out.println("Inserir: ");
 break;
 
 case "l"://Localitzar
@@ -99,9 +155,19 @@ if (cognoms[p] == null) {
     System.out.println("A la posició " +p+" es troba l'element: "+cognoms[p]+".");
 
 }
-
 break;
 
+
+case "p": //primerDarrer
+System.out.println("PrimerDarrer");
+  entradaLlista();
+  funcioPD(); //crido a la funció
+break;
+
+
+case "m"://Imprimir
+funcioImpr();
+break;
 
 case "x":
 System.out.println("Vinga dw");
